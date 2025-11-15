5397a286851b8765235210ea278172a033a5c394
Muhmd Mustafa <muhmdmustafa10@gmail.com>
2025-11-15 16:16:02 +0200

    feat: add DOM element references for product form fields and create button.

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,11 @@
-// main.js
+//Declare DOM element references
+let productName = document.getElementById("productName");
+let price = document.getElementById("price");
+let taxes = document.getElementById("taxes");
+let ads = document.getElementById("ads");
+let discount = document.getElementById("discount");
+let totalPrice = document.getElementById("totalPrice");
+let count = document.getElementById("count");
+let category = document.getElementById("category");
+let create = document.getElementById("create");
+// console.log(productName,price,taxes,ads,discount,totalPrice,count,category,create);
