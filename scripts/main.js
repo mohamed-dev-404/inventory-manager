1bacaa3afa1c1c01996992fedfb7f860a992857d
MohamedIbrahemHamad <mohammedhamad.dev@gmail.com>
2025-11-15 17:08:27 +0200

    feat: implement product creation with count support and save data to localStorage.

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,4 @@
-//Declare DOM element references
+//*Declare DOM element references
 let productName = document.getElementById("productName");
 let price = document.getElementById("price");
 let taxes = document.getElementById("taxes");
@@ -9,7 +9,8 @@ let count = document.getElementById("count");
 let category = document.getElementById("category");
 let create = document.getElementById("create");
 // console.log(productName,price,taxes,ads,discount,totalPrice,count,category,create);
-//! get Total Price
+
+//* get Total Price
 function getTotalPrice() {
     if (price.value != "") {
         let result = (+price.value + +taxes.value + +ads.value) - +discount.value;
@@ -20,3 +21,38 @@ function getTotalPrice() {
         totalPrice.style.background = "#DC2626";
     }
 } 
+
+//* create Poduct
+let productList;
+if (localStorage.products != null) {
+    productList = JSON.parse(localStorage.products);
+} else {
+    productList = [];
+}
+
+create.onclick = function () {
+    let newProduct = {
+        productName: productName.value,
+        price: price.value,
+        taxes: taxes.value,
+        ads: ads.value,
+        discount: discount.value,
+        totalPrice: totalPrice.innerHTML,
+        count: count.value,
+        category: category.value
+    };
+
+    if (count.value > 1) {
+        for (let i = 0; i < count.value; i++) {
+            productList.push(newProduct);
+        }
+    } else {
+        productList.push(newProduct);
+    }
+    
+    //? save local Storage
+    localStorage.setItem('products', JSON.stringify(productList));
+
+   //todo: clearInputs(); //clear input fields
+  //todo: showProducts(); //show products
+}
